5794926bcdd82346b431df0c08d37ca191d70188
csmsky <kenronquillo04@gmail.com>
2024-11-13 20:27:56 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,38 @@
+/* 
+Dec 1, 2024 23:59:59 
+*/
+
+function countdown() {
+    const launchDate = new Date("Dec 1, 2024 23:59:59").getTime();
+    const button = document.getElementById("lockBtn");
+
+
+    const interval = setInterval(() => {
+        const now = new Date().getTime();
+        const timeLeft = launchDate - now;
+
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            document.querySelector(".timer").innerHTML = "<h2>Please Proceed :)</h2>";
+
+            button.disabled = false;
+            button.innerText = "+1";
+            button.onclick = () => {
+                window.location.href = "UNINSTALL/index1.html";
+            };
+            return;
+        }
+
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+    }, 1000);
+}
+
+countdown();
